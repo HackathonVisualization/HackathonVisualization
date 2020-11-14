156c9047fb858d531c71011003b9c34fe4d50c33
OrangeX4 <318483724@qq.com>
2020-11-14 19:40:46 +0800

    Fix antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import {
-    DesktopOutlined,
     PieChartOutlined,
-    FileOutlined,
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
@@ -20,23 +18,17 @@ function App() {
                 <div className="logo" />
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                     <Menu.Item key="1" icon={<PieChartOutlined />}>
-                        Option 1
-            </Menu.Item>
-                    <Menu.Item key="2" icon={<DesktopOutlined />}>
-                        Option 2
-            </Menu.Item>
+                        Total
+                    </Menu.Item>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
                     <SubMenu key="sub1" icon={<UserOutlined />} title="User">
                         <Menu.Item key="3">Tom</Menu.Item>
                         <Menu.Item key="4">Bill</Menu.Item>
                         <Menu.Item key="5">Alex</Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-                        <Menu.Item key="6">Team 1</Menu.Item>
-                        <Menu.Item key="8">Team 2</Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="9" icon={<FileOutlined />}>
-                        Files
-            </Menu.Item>
                 </Menu>
             </Sider>
             <Layout className="site-layout">
@@ -48,7 +40,7 @@ function App() {
                     </Breadcrumb>
                     <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
                         Bill is a cat.
-            </div>
+                    </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
             </Layout>

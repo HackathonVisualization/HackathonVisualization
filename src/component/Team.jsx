ea4ac2454a7e60ea175bb6d5e11ec74669ae52fa
qqktr <qqktr@outlook.com>
2020-11-23 21:37:09 +0800

    first edit

---
+++ src/component/Team.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Breadcrumb } from 'antd'
+
+class Team extends React.Component {
+  render() {
+    return (
+      <div>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Team</Breadcrumb.Item>
+        </Breadcrumb>
+        {/* TODO */}
+      </div>
+    );
+  }
+}
+
+export default Team
---
+++ src/component/Total.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Breadcrumb } from 'antd'
+class Total extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Total</Breadcrumb.Item>
+        </Breadcrumb>
+        {/* TODO */}
+      </div>
+    );
+  }
+}
+
+export default Total

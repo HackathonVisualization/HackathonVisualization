ad1d0ad987b2180e5e15adbbdc4a365bddec51e6
OrangeX4 <318483724@qq.com>
2020-11-24 22:36:01 +0800

    Refactor TeamShow

--- src/component/TeamShow.tsx
+++ src/component/TeamShow.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Breadcrumb } from 'antd'
 import { Link } from 'react-router-dom';
 
+interface Props {
+    match: {
+        params: {
+            [name: string]: string
+        }
+    }
+}
 
-function TeamShow(props) {
+function TeamShow(props: Props) {
     return (
         <div>
             <Breadcrumb style={{ margin: '16px 0' }}>
